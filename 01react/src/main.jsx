5490b1ffe6cfb9340f88023cf471f3201d35ee45
mudassir-jmi <mudassirakhter420@gmail.com>
2023-11-10 13:18:43 +0530

    how react works under the hood at 10/11/23

--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const User = "Akhter";
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visite google",
+  User // evaluated expression or variable value
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  reactElement
+  // </React.StrictMode>,
+);
