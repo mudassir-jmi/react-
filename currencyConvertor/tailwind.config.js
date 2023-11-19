ed415564dcd6ebacb521aa693230baa9f4e94783
mudassir-jmi <mudassirakhter420@gmail.com>
2023-11-19 16:20:28 +0530

    currency convertor project completd

--- currencyConvertor/tailwind.config.js
+++ currencyConvertor/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "Arial", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Inconsolata", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
