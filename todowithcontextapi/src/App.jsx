ea1a33122efe403e32c795f86166c05c91861c8d
mudassir-jmi <mudassirakhter420@gmail.com>
2023-12-06 21:57:31 +0530

    setup new project todo using contextapi

---
+++ todowithcontextapi/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-white font-bold bg-orange-700">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
