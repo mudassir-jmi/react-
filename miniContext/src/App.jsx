4063818010ce426c96155070c52aaafce361573e
mudassir-jmi <mudassirakhter420@gmail.com>
2023-12-06 21:20:28 +0530

    update App.js and index.html file

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <UserContextProvider>
       <h1 className="text-4xl font-extrabold text-center text-gradient bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 p-5 text-white mb-8 rounded w-1/2 mx-auto">
-        Welcome to <span className="text-yellow-300">Login Context API</span>
+        <span className="text-yellow-300">Login with Context API</span>
       </h1>
       <Login />
       <Profile />
