16a3688d2501b2a704f7c8f125a63ba7ce306283
mudassir-jmi <mudassirakhter420@gmail.com>
2023-12-26 01:13:12 +0530

    update App.css and App.jsx

--- todowithcontextapi/src/App.jsx
+++ todowithcontextapi/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import { TodoProvider } from "./contexts";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
@@ -43,7 +42,7 @@ function App() {
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div className="bg-[#172842] min-h-screen min-w-full py-8">
+      <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
