b583b88008f33a01701189b1bdc1f4d9d7424deb
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-03 00:56:37 +0530

    making blog page using appwrite backend as a service

--- appwriteBlog/src/App.jsx
+++ appwriteBlog/src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <h1>Hello APPWrite!</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
